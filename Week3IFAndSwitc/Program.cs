454eb4187bb3b5d617e9c30d83cba558f03f2476
RoberTKTK <rober.shommet@gmail.com>
2023-01-04 16:39:56 +0200

    add week3 code

---
+++ Week3IFAndSwitc/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+Console.WriteLine("Fahrenheit to Celsius (A) or Celsius to Fahrenheit? (B)");
+
+char userOperator = char.Parse(Console.ReadLine());
+
+
+
+Console.WriteLine("Enter the number you wish to transfer");
+
+int Num = Int32.Parse(Console.ReadLine());
+
+
+switch (userOperator)
+
+{
+    case 'A':
+
+        FahrenheitToCelsius(Num);
+
+        break;
+    case 'B':
+
+        CelsiusToFahrenheit(Num);
+
+        break;
+
+    default:
+
+        Console.WriteLine("Invalid operator");
+
+        break;
+
+}
+
+
+
+
+
+static void FahrenheitToCelsius(int a)
+
+{
+
+    Console.WriteLine($"{(a - 32) * 5 / 9}");
+
+}
+
+static void CelsiusToFahrenheit(int a)
+
+    {
+
+        Console.WriteLine($"{(a * 9) / 5 + 32}");
+
+}
+
+
+
